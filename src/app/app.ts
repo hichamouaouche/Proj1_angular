99e487e0c6554795d6b448b56e826d8d4247fd9c
hichamouaouche <hichamouaouche@gmail.com>
2026-05-02 09:47:52 +0100

    first part

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { RouterOutlet, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [
+    RouterLink
+    ],
+
 })
 export class App {
-  protected readonly title = signal('Enset-App');
+   email : String = "hichamouaouche2002@gmail.com";
 }
